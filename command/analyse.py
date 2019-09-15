21efee8ed1fe73143661d1d0db45409e8bc1aaf1
kit0415 <simonyip0415@gmail.com>
2019-09-15 21:05:03 +0800

    minor change

--- command/analyse.py
+++ command/analyse.py
@@ -1,9 +1,7 @@
-import re
-import os
 import pandas as pd
 
 mycols = ["IP","Identity","Username","Date","Date 2","Request Method","Response Code","Bytes Out","Referer","User Agent"]
-av = pd.read_csv("access.txt",sep=" ", names=mycols)
+av = pd.read_csv("../data/access.log",sep=" ", names=mycols)
 df = pd.DataFrame({
     "1-IP":av["IP"],
     "2-Username":av["Username"],
