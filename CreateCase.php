d75a33b8d8bfb65526faf93ca944740934aa4a7e
yongqinn <36617236+yongqinn@users.noreply.github.com>
2019-09-30 11:43:07 +0800

    update GUI

--- CreateCase.php
+++ CreateCase.php
@@ -1,107 +1,102 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>
-    Security Analytics
-  </title>
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-  <!-- CSS Files -->
-  <link href="css/bootstrap.min.css" rel="stylesheet"/>
-  <link href="css/frame.css" rel="stylesheet"/>
-</head>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>
+      Security Analytics
+    </title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
+      name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <!-- CSS Files -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/frame.css" rel="stylesheet" />
+  </head>
 
-<body class="">
-  <div class="wrapper ">
-    <div class="sidebar" data-color="white" data-active-color="danger">
-      <!--
+  <body class="">
+    <div class="wrapper ">
+      <div class="sidebar" data-color="white" data-active-color="danger">
+        <!--
         Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
     -->
-    <div class="logo">
-        <a href="http://www.creative-tim.com" class="simple-text logo-mini">
-          <div class="logo-image-small">
-            <img src="../assets/img/logo-small.png">
-          </div>
-        </a>
-        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-        Protein
-          <!-- <div class="logo-image-big">
+        <div class="logo">
+          <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+            <div class="logo-image-small">
+              <img src="../assets/img/logo-small.png">
+            </div>
+          </a>
+          <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+            Protein
+            <!-- <div class="logo-image-big">
             <img src="../assets/img/logo-big.png">
           </div> -->
-        </a>
-      </div>
-      <div class="sidebar-wrapper">
-        <ul class="nav">
-          <li>
-            <a href="./index.php">
-              <i class="nc-icon nc-bank"></i>
-              <p>Dashboard</p>
-            </a>
-          </li>
-          <li class="active ">
-            <a href="./CreateCase.php">
-              <i class="nc-icon nc-diamond"></i>
-              <p>Create Case</p>
-            </a>
-          </li>
-          <li>
-            <a href="./ViewCase.php">
-              <i class="nc-icon nc-pin-3"></i>
-              <p>View Case</p>
-            </a>
-          </li>
-          <li>
-            <a href="./CreateCommand.php">
-              <i class="nc-icon nc-bell-55"></i>
-              <p>Create Command</p>
-            </a>
-          </li>
-          <li>
-            <a href="./ViewCommand.php">
-              <i class="nc-icon nc-single-02"></i>
-              <p>View Command</p>
-            </a>
-          </li>
-          <li>
-            <a href="./tables.html">
-              <i class="nc-icon nc-tile-56"></i>
-              <p>Table List</p>
-            </a>
-          </li>         
-        </ul>
-      </div>
-    </div>
-    <div class="main-panel">
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
-        <div class="container-fluid">
-          <div class="navbar-wrapper">
-            <div class="navbar-toggle">
-              <button type="button" class="navbar-toggler">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </button>
-            </div>
-            <a class="navbar-brand">ICT3204 - Security Analytics</a>
-          </div>
-          
+          </a>
         </div>
-      </nav>
-      <!-- End Navbar -->
-      <!-- <div class="panel-header panel-header-lg">
+        <div class="sidebar-wrapper">
+          <ul class="nav">
+            <li>
+              <a href="./index.php">
+                <i class="nc-icon nc-bank"></i>
+                <p>Dashboard</p>
+              </a>
+            </li>
+            <li class="active ">
+              <a href="./CreateCase.php">
+                <i class="nc-icon nc-diamond"></i>
+                <p>Create Case</p>
+              </a>
+            </li>
+            <li>
+              <a href="./ViewCase.php">
+                <i class="nc-icon nc-pin-3"></i>
+                <p>View Case</p>
+              </a>
+            </li>
+            <li>
+              <a href="./CreateCommand.php">
+                <i class="nc-icon nc-bell-55"></i>
+                <p>Create Command</p>
+              </a>
+            </li>
+            <li>
+              <a href="./ViewCommand.php">
+                <i class="nc-icon nc-single-02"></i>
+                <p>View Command</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="main-panel">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
+          <div class="container-fluid">
+            <div class="navbar-wrapper">
+              <div class="navbar-toggle">
+                <button type="button" class="navbar-toggler">
+                  <span class="navbar-toggler-bar bar1"></span>
+                  <span class="navbar-toggler-bar bar2"></span>
+                  <span class="navbar-toggler-bar bar3"></span>
+                </button>
+              </div>
+              <a class="navbar-brand">ICT3204 - Security Analytics</a>
+            </div>
+
+          </div>
+        </nav>
+        <!-- End Navbar -->
+        <!-- <div class="panel-header panel-header-lg">
 
   <canvas id="bigDashboardChart"></canvas>
 
 
 </div> -->
-      <div class="content">
-      <div class="col-md-8">
+        <div class="content">
+          <div class="col-md-8">
             <div class="card card-user">
               <div class="card-header">
                 <h5 class="card-title">Opening a New Case</h5>
@@ -111,20 +106,8 @@
                   <div class="row">
                     <div class="col-md-5 pr-1">
                       <div class="form-group">
-                        <label>Company (disabled)</label>
-                        <input type="text" class="form-control" disabled="" placeholder="Company" value="Creative Code Inc.">
-                      </div>
-                    </div>
-                    <div class="col-md-3 px-1">
-                      <div class="form-group">
-                        <label>Username</label>
-                        <input type="text" class="form-control" placeholder="Username" value="michael23">
-                      </div>
-                    </div>
-                    <div class="col-md-4 pl-1">
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" placeholder="Email">
+                        <label>Case ID</label>
+                        <input type="text" class="form-control">
                       </div>
                     </div>
                   </div>
@@ -142,14 +125,7 @@
                       </div>
                     </div>
                   </div>
-                  <div class="row">
-                    <div class="col-md-12">
-                      <div class="form-group">
-                        <label>Address</label>
-                        <input type="text" class="form-control" placeholder="Home Address" value="Melbourne, Australia">
-                      </div>
-                    </div>
-                  </div>
+
                   <div class="row">
                     <div class="col-md-4 pr-1">
                       <div class="form-group">
@@ -173,25 +149,25 @@
                   <div class="row">
                     <div class="col-md-12">
                       <div class="form-group">
-                        <label>About Me</label>
-                        <textarea class="form-control textarea">Oh so, your weak rhyme You doubt I'll bother, reading into it</textarea>
+                        <label>Comments</label>
+                        <textarea class="form-control textarea"></textarea>
                       </div>
                     </div>
                   </div>
                   <div class="row">
                     <div class="update ml-auto mr-auto">
-                      <button type="submit" class="btn btn-primary btn-round">Update Profile</button>
+                      <button type="submit" class="btn btn-primary btn-round">Upload Case</button>
                     </div>
                   </div>
                 </form>
               </div>
             </div>
           </div>
-        
+
+        </div>
       </div>
     </div>
-  </div>
-  
-</body>
+
+  </body>
 
 </html>
--- ViewCase.php
+++ ViewCase.php
@@ -1,201 +1,123 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <title>
-    Security Analytics
-  </title>
-  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
-  <!--     Fonts and icons     -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
-  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-  <!-- CSS Files -->
-  <link href="css/bootstrap.min.css" rel="stylesheet"/>
-  <link href="css/frame.css" rel="stylesheet"/>
-</head>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>
+      Security Analytics
+    </title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
+      name='viewport' />
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <!-- CSS Files -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/frame.css" rel="stylesheet" />
+  </head>
 
-<body class="">
-  <div class="wrapper ">
-    <div class="sidebar" data-color="white" data-active-color="danger">
-      <!--
+  <body class="">
+    <div class="wrapper ">
+      <div class="sidebar" data-color="white" data-active-color="danger">
+        <!--
         Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
     -->
-    <div class="logo">
-        <a href="http://www.creative-tim.com" class="simple-text logo-mini">
-          <div class="logo-image-small">
-            <img src="../assets/img/logo-small.png">
-          </div>
-        </a>
-        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-        Protein
-          <!-- <div class="logo-image-big">
+        <div class="logo">
+          <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+            <div class="logo-image-small">
+              <img src="../assets/img/logo-small.png">
+            </div>
+          </a>
+          <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+            Protein
+            <!-- <div class="logo-image-big">
             <img src="../assets/img/logo-big.png">
           </div> -->
-        </a>
-      </div>
-      <div class="sidebar-wrapper">
-        <ul class="nav">
-          <li>
-            <a href="./index.php">
-              <i class="nc-icon nc-bank"></i>
-              <p>Dashboard</p>
-            </a>
-          </li>
-          <li>
-            <a href="./CreateCase.php">
-              <i class="nc-icon nc-diamond"></i>
-              <p>Create Case</p>
-            </a>
-          </li>
-          <li class="active ">
-            <a href="./ViewCase.php">
-              <i class="nc-icon nc-pin-3"></i>
-              <p>View Case</p>
-            </a>
-          </li>
-          <li>
-            <a href="./CreateCommand.php">
-              <i class="nc-icon nc-bell-55"></i>
-              <p>Create Command</p>
-            </a>
-          </li>
-          <li>
-            <a href="./ViewCommand.php">
-              <i class="nc-icon nc-single-02"></i>
-              <p>View Command</p>
-            </a>
-          </li>
-          <li>
-            <a href="./tables.html">
-              <i class="nc-icon nc-tile-56"></i>
-              <p>Table List</p>
-            </a>
-          </li>         
-        </ul>
-      </div>
-    </div>
-    <div class="main-panel">
-      <!-- Navbar -->
-      <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
-        <div class="container-fluid">
-          <div class="navbar-wrapper">
-            <div class="navbar-toggle">
-              <button type="button" class="navbar-toggler">
-                <span class="navbar-toggler-bar bar1"></span>
-                <span class="navbar-toggler-bar bar2"></span>
-                <span class="navbar-toggler-bar bar3"></span>
-              </button>
-            </div>
-            <a class="navbar-brand">ICT3204 - Security Analytics</a>
-          </div>
-          
+          </a>
         </div>
-      </nav>
-      <!-- End Navbar -->
-      <!-- <div class="panel-header panel-header-lg">
+        <div class="sidebar-wrapper">
+          <ul class="nav">
+            <li>
+              <a href="./index.php">
+                <i class="nc-icon nc-bank"></i>
+                <p>Dashboard</p>
+              </a>
+            </li>
+            <li>
+              <a href="./CreateCase.php">
+                <i class="nc-icon nc-diamond"></i>
+                <p>Create Case</p>
+              </a>
+            </li>
+            <li class="active ">
+              <a href="./ViewCase.php">
+                <i class="nc-icon nc-pin-3"></i>
+                <p>View Case</p>
+              </a>
+            </li>
+            <li>
+              <a href="./CreateCommand.php">
+                <i class="nc-icon nc-bell-55"></i>
+                <p>Create Command</p>
+              </a>
+            </li>
+            <li>
+              <a href="./ViewCommand.php">
+                <i class="nc-icon nc-single-02"></i>
+                <p>View Command</p>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="main-panel">
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
+          <div class="container-fluid">
+            <div class="navbar-wrapper">
+              <div class="navbar-toggle">
+                <button type="button" class="navbar-toggler">
+                  <span class="navbar-toggler-bar bar1"></span>
+                  <span class="navbar-toggler-bar bar2"></span>
+                  <span class="navbar-toggler-bar bar3"></span>
+                </button>
+              </div>
+              <a class="navbar-brand">ICT3204 - Security Analytics</a>
+            </div>
+
+          </div>
+        </nav>
+        <!-- End Navbar -->
+        <!-- <div class="panel-header panel-header-lg">
 
   <canvas id="bigDashboardChart"></canvas>
-
-
 </div> -->
-      <div class="content">
-        <div class="row">
-          <div class="col-lg-3 col-md-6 col-sm-6">
-            <div class="card card-stats">
-              <div class="card-body ">
-                <div class="row">
-                  <div class="col-5 col-md-4">
-                    <div class="icon-big text-center icon-warning">
-                      <i class="nc-icon nc-globe text-warning"></i>
-                    </div>
-                  </div>
-                  <div class="col-7 col-md-8">
-                    <div class="numbers">
-                      <p class="card-category">Investigator</p>
-                      <p class="card-title">1
-                        <p>
-                    </div>
-                  </div>
+        <div class="content">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card ">
+                <div class="card-header ">
+                  <h5 class="card-title">List of cases</h5>
+                  <hr>
+                  <table style="width:100%">
+                    <tr>
+                      <th>Case ID</th>
+                      <th>Case Description</th>
+                      <th>Date uploaded</th>
+                    </tr>
+                    <tr>
+                      <td>retrieve id</td>
+                      <td>retrieve link</td>
+                      <td>retrieve date</td>
+                    </tr>
+
+                  </table>
                 </div>
-              </div>
-              <div class="card-footer ">
-                <hr>
-                <div class="stats">
-                  <i class="fa fa-refresh"></i> Update Now
+                <div class="card-body ">
+                  <canvas id=chartHours width="400" height="100"></canvas>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6 col-sm-6">
-            <div class="card card-stats">
-              <div class="card-body ">
-                <div class="row">
-                  <div class="col-5 col-md-4">
-                    <div class="icon-big text-center icon-warning">
-                      <i class="nc-icon nc-money-coins text-success"></i>
-                    </div>
-                  </div>
-                  <div class="col-7 col-md-8">
-                    <div class="numbers">
-                      <p class="card-category">Total Case</p>
-                      <p class="card-title">4
-                        <p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="card-footer ">
-                <hr>
-                <div class="stats">
-                  <i class="fa fa-calendar-o"></i> Last day
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6 col-sm-6">
-            <div class="card card-stats">
-              <div class="card-body ">
-                <div class="row">
-                  <div class="col-5 col-md-4">
-                    <div class="icon-big text-center icon-warning">
-                      <i class="nc-icon nc-vector text-danger"></i>
-                    </div>
-                  </div>
-                  <div class="col-7 col-md-8">
-                    <div class="numbers">
-                      <p class="card-category">Malware Count</p>
-                      <p class="card-title">23
-                        <p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="card-footer ">
-                <hr>
-                <div class="stats">
-                  <i class="fa fa-clock-o"></i> In the last hour
-                </div>
-              </div>
-            </div>
-          </div>
-         
-        </div>
-        <div class="row">
-          <div class="col-md-12">
-            <div class="card ">
-              <div class="card-header ">
-                <h5 class="card-title">Indicators of Compromise</h5>
-                <p class="card-category">24 Hours performance</p>
-              </div>
-              <div class="card-body ">
-                <canvas id=chartHours width="400" height="100"></canvas>
-              </div>
-              <div class="card-footer ">
-                <hr>
-                <div class="stats">
-                  <i class="fa fa-history"></i> Updated 3 minutes ago
+                <div class="card-footer ">
                 </div>
               </div>
             </div>
@@ -203,8 +125,7 @@
         </div>
       </div>
     </div>
-  </div>
-  
-</body>
+
+  </body>
 
 </html>
