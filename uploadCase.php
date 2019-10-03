e31bb5d662ad779b1379f07c0c448f0f53f52b36
Simon Yip Choon Kit <simonyip0415@gmail.com>
2019-10-03 16:12:51 +0800

    path change

--- uploadCase.php
+++ uploadCase.php
@@ -20,20 +20,21 @@ if (isset($_POST['submit'])) {
 
 exec("unzip data/".$filename." -d data/");
 exec("rm data/".$filename);
+$default_dir = "/opt/lampp/htdocs/sec_analyst/";
 $files = scandir("data/");
 for ($i=2;$i<count($files)-2;$i++){
     $fileExt = pathinfo($files[$i]);
     switch ($fileExt["extension"]){
         case "pcapng":
             echo "command is python LogAnalysis.py";
-            $command = "python command/LogAnalysis.py ".$fileExt["extension"]." data/".$fileExt["filename"].".".$fileExt["extension"]." 1 TCP";
+            $command = "python ".$default_dir."command/LogAnalysis.py ".$fileExt["extension"]." ".$default_dir."data/".$fileExt["filename"].".".$fileExt["extension"]." 1 TCP";
             echo $command."<br/>";
             shell_exec($command);
             break;
         case "log":
             if ($fileExt["filename"] == "access"){
                 echo "access log";
-                $command = "python command/analyse.py data/".$fileExt["filename"].".".$fileExt["extension"];
+                $command = "python ".$default_dir."command/analyse.py"." ".$default_dir."data/".$fileExt["filename"].".".$fileExt["extension"];
                 echo $command."<br/>";
                 shell_exec($command);
             }
