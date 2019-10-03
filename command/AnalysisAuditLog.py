f7e6567b345c03e49f43ad01e4c7e9b52d2e5a72
Tan Yee Chow <yeechow1212@gmail.com>
2019-10-03 17:14:48 +0800

    update

--- command/AnalysisAuditLog.py
+++ command/AnalysisAuditLog.py
@@ -38,6 +38,7 @@ def AnalysisAL(auditlogfile, ignoreroot):
                 exe = find_between(line, "exe=", " ")
                 nametype = find_between(line, "nametype=", " ")
                 workingdir = find_between(line, "cwd=", " ")
+                inode = find_between(line, "inode=", " ")
 
 
                 proctitle = "-"
@@ -50,7 +51,7 @@ def AnalysisAL(auditlogfile, ignoreroot):
                 if (proctitle == "/usr/bin/nautilus" or exe == "/usr/bin/nautilus" or  "/usr/bin/nautilus" in proctitle):
                     continue
 
-                df = df.append({'timestamp' : timestamp , "command" : proctitle, 'path' : name, 'uid' : uid, 'exe' : exe, 'nametype' : nametype, 'workingdirectory' : workingdir} , ignore_index=True)
+                df = df.append({'timestamp' : timestamp , "command" : proctitle, 'path' : name, 'uid' : uid, 'exe' : exe, 'inode': inode, 'nametype' : nametype, 'workingdirectory' : workingdir} , ignore_index=True)
 
 
 
@@ -62,6 +63,7 @@ def printFullAL(df):
 def printSuspiciousAuditActivity(df):
     image_ext_list = ['jpg', 'png', 'jpeg', 'TIF', 'gif']
     first_suspicious_date = "null"
+    file_list = []
 
     #find suspicious upload file -> not image
     print "Non-Image Suspicious files Created:"
@@ -77,14 +79,51 @@ def printSuspiciousAuditActivity(df):
             else:
                 if (first_suspicious_date == "null"):
                     first_suspicious_date = row['timestamp']
-                print filename
+                file_list.append(filename)
+
+    file_list = list(dict.fromkeys(file_list))
+    for file in file_list:
+        print file
+
     return first_suspicious_date
+
+def printSuspiciousFileList(df):
+    image_ext_list = ['jpg', 'png', 'jpeg', 'TIF', 'gif']
+    first_suspicious_date = "null"
+    file_list = []
+
+    #find suspicious upload file -> not image
+    print "Non-Image Suspicious files Created:"
+    for index, row in df.iterrows():
+        #Check for upload files
+        if ("/var/www/html/sa/3204/uploads/" in row['path']):
+            filename = row['path'].replace("/var/www/html/sa/3204/uploads/", "")
+            filename = filename.replace(" ", "")
+
+            extension = filename[filename.index(".") + len("."):].lower()
+            if (extension in image_ext_list):
+                continue
+            else:
+                if (first_suspicious_date == "null"):
+                    first_suspicious_date = row['timestamp']
+                file_list.append(filename)
+
+    file_list = list(dict.fromkeys(file_list))
+    for file in file_list:
+        print file
+
+    return file_list
 
 #print command executed
 def printCommandExecutedBySuspTime(df, first_susp_timestamp):
     search = False
+    first_susp_timestamp_dt = datetime.strptime(first_susp_timestamp, "%m/%d/%Y %H:%M:%S")
+
     for index, row in df.iterrows():
-        if row['timestamp'] == first_susp_timestamp:
+        ts = row['timestamp']
+        ts = ts.split(".", 1)[0]
+        ts_dt = datetime.strptime(ts, "%m/%d/%Y %H:%M:%S")
+        if ts_dt >= first_susp_timestamp_dt:
            search = True
 
         if (search == True):
@@ -96,9 +135,32 @@ def printModifiedFiles(df, first_susp_timestamp):
     print "\nPossible Files Modified:"
     linux_mod_commands = [' nano ', ' gedit ', ' vim ', ' vi ', ' echo ', ' emacs ']
     search = False
+    first_susp_timestamp_dt = datetime.strptime(first_susp_timestamp, "%m/%d/%Y %H:%M:%S")
 
     for index, row in df.iterrows():
-        if row['timestamp'] == first_susp_timestamp:
+        ts = row['timestamp']
+        ts = ts.split(".", 1)[0]
+        ts_dt = datetime.strptime(ts, "%m/%d/%Y %H:%M:%S")
+        if ts_dt >= first_susp_timestamp_dt:
+            search = True
+
+        if (search == True):
+            if row['command'] != '-' and "/usr/sbin/apache2 -k start" not in row['command']:
+                for mod_com in linux_mod_commands:
+                    if(mod_com in row['command']):
+                        print row['timestamp']+ " " + row['command']
+
+def printCreatedFiles(df, first_susp_timestamp):
+    print "\nPossible Files Created:"
+    linux_mod_commands = [' touch ', ' cp ', ' cat ', ' nano ', ' gedit ', ' vim ', ' vi ', ' echo ', ' emacs ']
+    search = False
+    first_susp_timestamp_dt = datetime.strptime(first_susp_timestamp, "%m/%d/%Y %H:%M:%S")
+
+    for index, row in df.iterrows():
+        ts = row['timestamp']
+        ts = ts.split(".", 1)[0]
+        ts_dt = datetime.strptime(ts, "%m/%d/%Y %H:%M:%S")
+        if ts_dt >= first_susp_timestamp_dt:
             search = True
 
         if (search == True):
@@ -111,9 +173,13 @@ def printDeletedFiles(df, first_susp_timestamp):
     print "\nPossible Files Deleted:"
     linux_delete_commands = [' rm ', ' unlink ', ' rmdir ']
     search = False
+    first_susp_timestamp_dt = datetime.strptime(first_susp_timestamp, "%m/%d/%Y %H:%M:%S")
 
     for index, row in df.iterrows():
-        if row['timestamp'] == first_susp_timestamp:
+        ts = row['timestamp']
+        ts = ts.split(".", 1)[0]
+        ts_dt = datetime.strptime(ts, "%m/%d/%Y %H:%M:%S")
+        if ts_dt >= first_susp_timestamp_dt:
             search = True
 
         if (search == True):
@@ -122,16 +188,34 @@ def printDeletedFiles(df, first_susp_timestamp):
                     if(mod_com in row['command']):
                         print row['timestamp'] + " " + row['command']
 
+def getFirstBruteForceTimeFromAccessLog():
 
+    df = pd.read_csv("abc.csv", sep=",")
+    df.columns = ["Index", "IP", "Username", "Date", "Request Method", "Response Code", "Bytes Out", "Referer",
+                  "User Agent"]
+    for index, row in df.iterrows():
+        return row['Date']
+
+
+
+first_bf_timestamp = getFirstBruteForceTimeFromAccessLog()
+first_bf_timestamp= first_bf_timestamp.replace('[','')
+first_bf_timestamp = first_bf_timestamp.split("+")[0]
+first_bf_timestamp = re.sub(":"," ",first_bf_timestamp,count=1)
+first_bf_timestamp_dt = datetime.strptime(first_bf_timestamp, '%d/%b/%Y %H:%M:%S')
+first_bf_timestamp_dt = first_bf_timestamp_dt.strftime("%m/%d/%Y %H:%M:%S")
 
 auditlog_df = AnalysisAL("../data/ausearch.log", True)
-first_susp_timestamp = printSuspiciousAuditActivity(auditlog_df)
+first_susp_file_timestamp = printSuspiciousAuditActivity(auditlog_df)
 
-print "First suspicious timestamp: " + first_susp_timestamp + "\n"
+print "First suspicious file timestamp: " + first_susp_file_timestamp + "\n"
 
-print "Suspicious command from " + first_susp_timestamp + ":\n"
-printCommandExecutedBySuspTime(auditlog_df, first_susp_timestamp)
+print "Suspicious command from " + first_bf_timestamp_dt + ":\n"
+printCommandExecutedBySuspTime(auditlog_df, first_bf_timestamp_dt)
 
-printModifiedFiles(auditlog_df, first_susp_timestamp)
+printCreatedFiles(auditlog_df, first_bf_timestamp_dt)
 
-printDeletedFiles(auditlog_df, first_susp_timestamp)
+printModifiedFiles(auditlog_df, first_bf_timestamp_dt)
+
+printDeletedFiles(auditlog_df, first_bf_timestamp_dt)
+
